4d9ee6d655c6fda02b51eaf72598fa0240f24f9e
Pedro Alberto Veber Berna <pedro.veber@outlook.com>
2022-11-11 23:27:01 -0300

    feat: project initialized

---
+++ index.js
@@ -0,0 +1,8 @@
+/* external modules */
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+/* internal modules */
+const fs = require("fs");
+
+console.log("Iniciamos o projeto!!");
